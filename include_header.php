4855591fbe992dfca3860bf49d96b4a5da99dfac
ElenaGir <elena.girard@gmail.com>
2022-04-13 15:49:49 +0200

    adding database sql

--- include_header.php
+++ include_header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="backoffice for my portfolio, developped in php & mysql">
+    <meta name="description" content="backoffice for my portfolio-blog, developped in php & mysql">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
